a89e6d6a2f5e5710bf64b4d38d8238431b67cf1f
CloudCodeX User <cloudcodex@local>
2026-03-26 18:17:57 +0000

    Initial commit

---
+++ wtest/test.py
@@ -0,0 +1,9 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Example usage
+print(check_even_odd(7))  # Output: Odd
+print(check_even_odd(4))  # Output: Even   
